2277056dec0dab0fbccaa95ed2e656c9658f24f9
Patrick <ptrollip@gmail.com>
2021-01-05 10:29:21 +0000

    Fininshed tutorial on youtube to practice redux and build this basic counter app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React  from 'react';
+import { useSelector,useDispatch } from 'react-redux'; //Allows us to select a reducer to use in the app.
+import { increment, decrement } from './actions';
 
 function App() {
+  const counter = useSelector(state => state.counter);
+  const isLogged = useSelector(state => state.isLogged);
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <h1>Counter {counter}</h1>
+      <button onClick={() => dispatch(increment(2))}>+</button> 
+      <button onClick={() => dispatch(decrement())}>-</button>
+      {isLogged ? <h3>I should not see this if user is not logged in.</h3> : ''}
     </div>
   );
 }
